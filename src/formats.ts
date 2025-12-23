763c8bdf06ebd6a466b85479f0999dfdf486bb18
cookieukw <cookieukw@users.noreply.github.com>
2025-12-22 21:19:15 -0300

    fix: correct character and prop name translations in setupFormats

--- src/formats.ts
+++ src/formats.ts
@@ -49,8 +49,8 @@ export function setupFormats() {
     };
 
     let format_character = new ModelFormat("hytale_character", {
-      name: t("formats.prop.name"),
-      description: t("formats.prop.description"),
+      name: t("formats.character.name"),
+      description: t("formats.character.description"),
       icon: "icon-format_hytale",
       format_page,
       block_size: 64,
@@ -64,8 +64,8 @@ export function setupFormats() {
         */
     });
     let format_prop = new ModelFormat("hytale_prop", {
-      name: t("formats.character.name"), 
-      description: t("formats.character.description"), 
+      name: t("formats.prop.name"), 
+      description: t("formats.prop.description"), 
       icon: "icon-format_hytale",
       format_page,
       block_size: 64,
